edea3bd01dd815d2e25bbb8a5f1665c638ab3219
AllanSantanna <allanxlink@hotmail.com>
2024-04-26 16:58:11 -0300

    fix: 'dashborad routes and controllers'

--- src/routes/dashboard.routes.ts
+++ src/routes/dashboard.routes.ts
@@ -1,27 +1,8 @@
 import express, { Request, Response } from 'express'
-import * as transactionRepository from '../repository/transaction'
-import * as util from '../utils'
+import * as dashboardController from '../controllers/dashboardController'
 
 const dashboardRouter = express.Router()
 
-dashboardRouter.get('/dashboard', async (req: Request, res: Response) => {
-  const userId = req.params.userId
-
-  try {
-    const transactions = await transactionRepository.getTransactionByUserId(+userId)
-
-    if (!transactions || transactions.length === 0) {
-      return res
-        .status(404)
-        .json({ message: 'No transactions found for this user.' })
-    }
-
-    const dashboardData = util.calculateDashboardData(transactions)
-    res.json(dashboardData)
-  } catch (error) {
-    console.error('Error fetching transactions:', error)
-    res.status(500).json({ message: 'Internal server error.' })
-  }
-})
+dashboardRouter.get('/dashboard', dashboardController.getDashboardData)
 
 export default dashboardRouter
--- src/utils/calculateDashboardData.ts
+++ src/utils/calculateDashboardData.ts
@@ -1,29 +1,36 @@
-import {
-  TRANSACTION_TYPE,
-} from '../consts/transaction'
+import { TRANSACTION_TYPE } from '../consts/transaction'
 
 import { TransactionModel } from '../model/transaction'
 
 export const calculateTest = () => {}
 
 export const calculateDashboardData = (transactions: TransactionModel[]) => {
-  const essencialTransactions = transactions
-    .filter(transaction => transaction.type === TRANSACTION_TYPE.ESSENTIAL)
+  const essencialTransactions = transactions.filter(
+    transaction => transaction.type === TRANSACTION_TYPE.ESSENTIAL
+  )
 
-  const superfluousTransactions = transactions
-    .filter(transaction => transaction.type === TRANSACTION_TYPE.SUPERFLUOUS)
+  const superfluousTransactions = transactions.filter(
+    transaction => transaction.type === TRANSACTION_TYPE.SUPERFLUOUS
+  )
 
-  const balanceTransactions = transactions
-    .filter(transaction => transaction.type === TRANSACTION_TYPE.BALANCE)
+  const balanceTransactions = transactions.filter(
+    transaction => transaction.type === TRANSACTION_TYPE.BALANCE
+  )
 
-  const essentialExpensesTotal = essencialTransactions
-    .reduce((total, transaction) => total + transaction.value, 0)
+  const essentialExpensesTotal = essencialTransactions.reduce(
+    (total, transaction) => total + transaction.value,
+    0
+  )
 
-  const nonEssentialExpensesTotal = superfluousTransactions
-    .reduce((total, transaction) => total + transaction.value, 0)
+  const nonEssentialExpensesTotal = superfluousTransactions.reduce(
+    (total, transaction) => total + transaction.value,
+    0
+  )
 
-  const totalIncome = balanceTransactions
-    .reduce((total, transaction) => total + transaction.value, 0)
+  const totalIncome = balanceTransactions.reduce(
+    (total, transaction) => total + transaction.value,
+    0
+  )
 
   const balance =
     totalIncome - essentialExpensesTotal - nonEssentialExpensesTotal
@@ -32,23 +39,22 @@ export const calculateDashboardData = (transactions: TransactionModel[]) => {
 
   const emergencyReserve = essentialExpensesTotal * 12
 
-  const incomeDetails = balanceTransactions
-    .map(transaction => ({
-      name: transaction.description,
-      value: transaction.value
-    }))
+  const incomeDetails = balanceTransactions.map(transaction => ({
+    name: transaction.description,
+    value: transaction.value
+  }))
 
-  const essentialExpenseDetails = essencialTransactions
-    .map(transaction => ({
-      name: transaction.description,
-      value: transaction.value
-    }))
+  const essentialExpenseDetails = essencialTransactions.map(transaction => ({
+    name: transaction.description,
+    value: transaction.value
+  }))
 
-  const nonEssentialExpenseDetails = superfluousTransactions
-    .map(transaction => ({
+  const nonEssentialExpenseDetails = superfluousTransactions.map(
+    transaction => ({
       name: transaction.description,
       value: transaction.value
-    }))
+    })
+  )
 
   return {
     totalIncome,
@@ -62,4 +68,3 @@ export const calculateDashboardData = (transactions: TransactionModel[]) => {
     emergencyReserve
   }
 }
-
