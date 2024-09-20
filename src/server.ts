f1e5221ddbdf2a1304664a7271991b68d79a1f59
AllanSantanna <allanxlink@hotmail.com>
2024-09-20 17:49:10 -0300

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
+import cors from 'cors'
 import routes from './routes'
 
 dotenv.config()
 
 const app = express()
-const port = process.env.PORT || 3001
+const port = process.env.BACKEND_PORT || 3001
+
+app.use(cors())
 
 app.use(express.json())
 
